a76a70f16037ff6fe146ddd2b4bfe78cc7ed331a
Luís Filipe Esperança de Abreu <lfeabreu@gmail.com>
2023-01-26 21:07:41 -0300

    Aula 2 completada

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,10 +1,18 @@
 <?php
 
+require 'vendor/autoload.php';
+require 'src/Buscador.php';
+
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+use Alura\BuscadorDeCursos\Buscador;
 
-$client = new \GuzzleHttp\Client();
-$resposta = $client->request('GET', 'http://alura.com.br/cursos-online-programacao/php');
+$client = new Client(['base_uri' => 'http://www.alura.com.br']);
+$crawler = new Crawler();
 
-$html = $resposta->getBody();
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
 
-
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
